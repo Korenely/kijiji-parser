5b96ee2bb9edef7a02856c6c5b2948fcf2ab2d95
Korenely <kornelydorius@gmail.com>
2022-09-08 15:58:18 +0300

    Added date parser

--- parse.py
+++ parse.py
@@ -1,7 +1,11 @@
 import re
 import requests
+import dateparser
 from bs4 import BeautifulSoup
 from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine
+
+NAMBER_OF_PAGES = 94
+
 meta = MetaData()
 
 kijiji_elements = Table(
@@ -11,8 +15,8 @@ kijiji_elements = Table(
     Column('description', String),
     Column('location', String),
     Column('image', String),
-    Column('data', String),
-    Column('bedrooms', Integer),
+    Column('date', String),
+    Column('beds', Integer),
     Column('currency', String),
     Column('price', String),
 )
@@ -24,17 +28,19 @@ meta.create_all(engine)
 def parse_single_element(element_soup: BeautifulSoup):
     conn = engine.connect()
     try:
-        bedrooms = int(re.findall("\d+", element_soup.select_one(".bedrooms").text.strip())[0])
+        beds = int(re.findall("\d+", element_soup.select_one(".beds").text.strip())[0])
     except IndexError:
-        bedrooms = 0
+        beds = 0
+
     description = element_soup.select_one(".description").text.strip().replace("\n", "").replace("  ", "")
+    date = dateparser.parse(element_soup.select_one(".date-posted").text.strip()).strftime("%d-%m-%Y")
     kijiji_elements_add = kijiji_elements.insert().values(
         title=element_soup.select_one(".title").text.strip(),
         description=description,
         location=element_soup.select_one(".location > span").text.strip(),
         image=element_soup.select_one("img")["src"].strip(),
-        data=element_soup.select_one(".date-posted").text.strip(),
-        bedrooms=bedrooms,
+        date=date,
+        beds=beds,
         currency=element_soup.select_one(".price").text.strip()[0].strip(),
         price=element_soup.select_one(".price").text.strip()[1:],
     )
@@ -42,14 +48,13 @@ def parse_single_element(element_soup: BeautifulSoup):
 
 
 def main():
-    NAMBER_OF_PAGES = 94
     i = 0
     while i < NAMBER_OF_PAGES:
         i = i + 1
         page = requests.get(f"https://www.kijiji.ca/b-apartments-condos/city-of-toronto/page-{i}/c37l1700273").content
         soup = BeautifulSoup(page, "html.parser")
         elements = soup.select(".top-feature")
-        print([parse_single_element(element_soup) for element_soup in elements])
+        return [parse_single_element(element_soup) for element_soup in elements]
 
 
 if __name__ == "__main__":
